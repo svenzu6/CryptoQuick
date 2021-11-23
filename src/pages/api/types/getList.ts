faf1c241c46ae790c9aed4817e07ca6683056150
svenzu6 <zubcic.sven@gmail.com>
2021-11-23 09:39:56 +0100

    ref(getList): adjust types

--- src/pages/api/types/getList.ts
+++ src/pages/api/types/getList.ts
@@ -1,9 +1,9 @@
-export type GetListType = {
+export type GetListType= Array<{
     image: string
-    market_cap_change_24h: number
+    market_cap_change_percentage_24h: number
     market_cap_rank: number
     name: string
     current_price: number
     market_cap: number
     id: string
-}
+}>
